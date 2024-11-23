7e01f209ec70edfc5e1a1b066a6c06963b183c7d
gambolkf <gambolkf@gmail.com>
2024-11-23 23:39:16 +0100

    fix deply configuration

--- code/elastic_utils/utils.py
+++ code/elastic_utils/utils.py
@@ -1,5 +1,10 @@
 from elasticsearch import Elasticsearch
+import os
 
-es_client = Elasticsearch("https://localhost:9200/", basic_auth=("elastic", "lBHjhTepe*UVjBYEKhyz"), verify_certs=False)
+es_client = (
+    Elasticsearch("https://localhost:9200/", basic_auth=("elastic", "lBHjhTepe*UVjBYEKhyz"), verify_certs=False)
+    if os.path.exists(".env") else
+    Elasticsearch("http://elasticsearch-master.elk.svc.cluster.local:9200", basic_auth="elastic", verify_certs=False)
+)
 
 
