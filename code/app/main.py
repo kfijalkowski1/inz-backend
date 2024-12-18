b0f2793594b2b3d45786cf8787080c80dbd84d70
kfijalkowski1 <k.1.fijalkowski@gmail.com>
2024-12-18 14:44:47 +0100

    prod cors * disabled

--- code/app/main.py
+++ code/app/main.py
@@ -27,13 +27,13 @@ app.include_router(estates_router.router)
 app.include_router(workers_router.router)
 app.include_router(requests_router.router)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware( # TODO test for prod?
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 # workaround for creating estates and admins
 create_estate_if_not_exists(get_db())
